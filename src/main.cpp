8e16a22ac696fbc9395e5171b60c3721a7cda713
Øystein Bringsli <oysteinmb@gmail.com>
2024-07-01 18:36:05 +0200

    Ehh, whatever

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,24 @@
 #include <Arduino.h>
-#include <OneWire.h>
-#include <array>
 
 #include "stepper.h"
 #include "timerStats.hpp"
 
-OneWire oneWire(13);
 
 Stepper stepper{26, 27, 32, 33, 34};
 TimerStats timerStats;
-std::array<bool, 5> activations;
-
-
-enum State {
-    OFF,
-    Clockwise,
-    Double,
-    AntiClockwise
-};
-
-void push_back(bool e) {
-    for (int i = 0; i < activations.size() - 1; i++) {
-        activations[i] = activations[i + 1];
-    }
-
-    activations[activations.size() - 1] = e;
-}
-
-State compare(byte data) {
-    switch (data) {
-        case 1:
-        {
-            return State::Clockwise;
-        } break;
-
-        case 2:
-        {
-            return State::Double;
-        } break;
-
-        case 3:
-        {
-            return State::AntiClockwise;
-        } break;
-
-        default:
-        {
-            return State::OFF;
-        }
-    }
-}
 
 void setup() {
     Serial.begin(9600);
     Serial.println();
     Serial.println("Serial configured.");
+
+    delay(5000);
 }
 
 void loop() {
 
-    byte data;
-    if (oneWire.reset()) {
-        data = oneWire.read();
-        Serial.print("Reading data: ");
-        Serial.println(data);
-    } else {
-        return;
-    }
-
-    auto s = compare(data);
-
-    switch (s) {
-        case State::Clockwise:
-        {
-            stepper.turnQuart(true);
-        } break;
-
-        case State::AntiClockwise:
-        {
-            stepper.turnQuart(false);
-        }
-
-        case State::Double:
-        {
-            stepper.turnHalf();
-        }
+    if (digitalRead(13)) {
+        Serial.println("Turning.");
+        stepper.turnQuart(true);
     }
 }
--- src/stepper.cpp
+++ src/stepper.cpp
@@ -5,81 +5,187 @@
 #include <Arduino.h>
 #include "stepper.h"
 
+#define maxPWM  255
+#define threeFourthsPWM 180
+#define halfPWM 128
+#define minPWM    0
+
+void setupPWM(int pin, int channel) {
+    ledcSetup(channel, 10000, 8);
+    ledcAttachPin(pin, channel);
+}
+
 Stepper::Stepper(int pin0, int pin1, int pin2, int pin3, int enable)
     :pin0(pin0), pin1(pin1), pin2(pin2), pin3(pin3), enablePin(enable) {
 
-    pinMode(pin0, OUTPUT);
-    pinMode(pin1, OUTPUT);
-    pinMode(pin2, OUTPUT);
-    pinMode(pin3, OUTPUT);
-    pinMode(enablePin, OUTPUT);
+    setupPWM(pin0, 0);
+    setupPWM(pin1, 1);
+    setupPWM(pin2, 2);
+    setupPWM(pin3, 3);
 
-    digitalWrite(enablePin, OUTPUT);
+    pinMode(enablePin, OUTPUT);
+    digitalWrite(enablePin, HIGH); // Was set to "OUTPUT" previously, might be error.
 }
 
 void Stepper::step(bool forward) {
 
     if (forward) {
         currentStep++;
-        if (currentStep == 4) {
+        if (currentStep == 16) {
             currentStep = 0;
         }
     } else {
         currentStep--;
         if (currentStep == -1) {
-            currentStep = 3;
+            currentStep = 15;
         }
     }
 
     switch (currentStep) {
         case 0:
         {
-            digitalWrite(pin0, HIGH);
-            digitalWrite(pin1, LOW);
-            digitalWrite(pin2, HIGH);
-            digitalWrite(pin3, LOW);
+            ledcWrite(0, maxPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, maxPWM);
+            ledcWrite(3, minPWM);
         } break;
 
         case 1:
         {
-            digitalWrite(pin0, LOW);
-            digitalWrite(pin1, HIGH);
-            digitalWrite(pin2, HIGH);
-            digitalWrite(pin3, LOW);
+            ledcWrite(0, threeFourthsPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, maxPWM);
+            ledcWrite(3, minPWM);
         } break;
 
         case 2:
         {
-            digitalWrite(pin0, LOW);
-            digitalWrite(pin1, HIGH);
-            digitalWrite(pin2, LOW);
-            digitalWrite(pin3, HIGH);
+            ledcWrite(0, minPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, maxPWM);
+            ledcWrite(3, minPWM);
         } break;
 
         case 3:
         {
-            digitalWrite(pin0, HIGH);
-            digitalWrite(pin1, LOW);
-            digitalWrite(pin2, LOW);
-            digitalWrite(pin3, HIGH);
+            ledcWrite(0, minPWM);
+            ledcWrite(1, threeFourthsPWM);
+            ledcWrite(2, maxPWM);
+            ledcWrite(3, minPWM);
+        } break;
+
+        case 4:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, maxPWM);
+            ledcWrite(2, maxPWM);
+            ledcWrite(3, minPWM);
+        } break;
+
+        case 5:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, maxPWM);
+            ledcWrite(2, threeFourthsPWM);
+            ledcWrite(3, minPWM);
+        } break;
+
+        case 6:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, maxPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, minPWM);
+        } break;
+
+        case 7:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, maxPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, threeFourthsPWM);
+        } break;
+
+        case 8:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, maxPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, maxPWM);
+        } break;
+
+        case 9:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, threeFourthsPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, maxPWM);
+        } break;
+
+        case 10:
+        {
+            ledcWrite(0, minPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, maxPWM);
+        } break;
+
+        case 11:
+        {
+            ledcWrite(0, threeFourthsPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, maxPWM);
+        } break;
+
+        case 12:
+        {
+            ledcWrite(0, maxPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, maxPWM);
+        } break;
+
+        case 13:
+        {
+            ledcWrite(0, maxPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, threeFourthsPWM);
+        } break;
+
+        case 14:
+        {
+            ledcWrite(0, maxPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, minPWM);
+            ledcWrite(3, minPWM);
+        } break;
+
+        case 15:
+        {
+            ledcWrite(0, maxPWM);
+            ledcWrite(1, minPWM);
+            ledcWrite(2, threeFourthsPWM);
+            ledcWrite(3, minPWM);
         } break;
 
         default:
         {
-            throw std::runtime_error("Step count must be element in [0, 1, 2, 3].");
+            throw std::runtime_error("Step count must be element in range 0 to 15.");
         }
     }
 }
 
 void Stepper::turnQuart(bool forward) {
-    for (int w = 0; w < 50; w++){
+    for (int w = 0; w < 200; w++){
         step(forward);
         delay(pauseTime);
     }
 }
 
 void Stepper::turnHalf() {
-    for (int w = 0; w < 100; w++) {
+    for (int w = 0; w < 400; w++) {
         step(true);
         delay(pauseTime);
     }
